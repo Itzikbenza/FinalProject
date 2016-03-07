cccfbe3d2da0cce85a92e476ea274cdfe0c2ac0f
Itzikbenza <itzikbenza@gmail.com>
2016-03-07 17:25:32 +0200

    ytr
    rty

---
+++ FinalProject/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FinalProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            string text;
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                text = File.ReadAllText(openFileDialog.FileName);
+                txtEditor.Text = text;
+
+                string[] lines = text.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.None);
+             
+
+            }
+               
+        }
+
+    }
+}
