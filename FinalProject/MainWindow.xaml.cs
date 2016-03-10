11675d11663480be0ef2aab68801465cf6ff6468
yosi <yos35216@gmail.com>
2016-03-10 14:59:04 +0200

    gui change
    t

--- FinalProject/MainWindow.xaml.cs
+++ FinalProject/MainWindow.xaml.cs
@@ -13,29 +13,44 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Navigation;
+
+
+
+
 
 namespace FinalProject
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Window1.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Window1 : Window
     {
-        public MainWindow()
+        public Window1()
         {
-
             InitializeComponent();
+            
         }
         public float[][] Jdistance; //matrix of all jaccard distance values!
         public int linesNumber; //size of rows
         public string[][] FileMatrix; //matrix of the file readed
         public int threadCounter = 3; //number of thread
 
-        public void calc_JDistance (int start, int end, int numOfThread)
+        private void Open_File_Click(object sender, RoutedEventArgs e)
         {
-            IEnumerable<string> union, intersect;
+            string FileBuff; //buffer for read the file
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                FileBuff = File.ReadAllText(openFileDialog.FileName);
+                txtEditor.Text = FileBuff; //show the file on txt editor
+            }
+        }
+
+        private void jccard_button_Click(object sender, RoutedEventArgs e)
+        {
+             IEnumerable<string> union, intersect;
             float interCount = 0, unionCount = 0;
             float[][] tempDis;
             string[][] temp;
@@ -72,45 +87,7 @@ namespace FinalProject
                     UiInvoke(() => txtEditor.Text = String.Join(" ", Jdistance[0].Select(p => p.ToString()).ToArray()));
                 }
 
-            }
-        }
-        private void btnOpenFile_Click(object sender, RoutedEventArgs e)
-        {
-            string FileBuff; //buffer for read the file
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            if (openFileDialog.ShowDialog() == true)
-            {
-                FileBuff = File.ReadAllText(openFileDialog.FileName);
-                txtEditor.Text = FileBuff; //show the file on txt editor
-
-                string[] lines = FileBuff.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.None);
-                FileMatrix = new string[lines.Length][];
-
-                for (int i = 0; i < lines.Length; i++)
-                {
-                    FileMatrix[i] = lines[i].Split(new string[] { "\t", " " }, StringSplitOptions.None);
-                }
-
-                Jdistance = new float[lines.Length][];
-                linesNumber = lines.Length;
-                //Thread creation
-                Thread tt1 = new Thread(() => calc_JDistance(0, linesNumber / 5, 1));
-                Thread tt2 = new Thread(() => calc_JDistance(linesNumber / 5, (linesNumber / 5) * 2, 2));
-                Thread tt3 = new Thread(() => calc_JDistance((linesNumber / 5) * 2, linesNumber, 3));
-          
-                tt1.Start();
-                tt2.Start();
-                tt3.Start();
-            }
-
-        }
-        //INVOKE
-        public static void UiInvoke(Action a)
-        {
-            Application.Current.Dispatcher.Invoke(a);
         }
 
-        private Object thisLock = new Object(); //object lock critical section
-        
     }
 }
