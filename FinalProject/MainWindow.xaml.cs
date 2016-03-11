3b357b34dcc4a81b7cd224c07d3adb34fa8b7657
Itzikbenza <itzikbenza@gmail.com>
2016-03-11 22:53:02 +0200

    תיקונים אחרי GUI

--- FinalProject/MainWindow.xaml.cs
+++ FinalProject/MainWindow.xaml.cs
@@ -30,16 +30,23 @@ namespace FinalProject
         public Window1()
         {
             InitializeComponent();
-            
+
         }
         public float[][] Jdistance; //matrix of all jaccard distance values
         public int linesNumber; //size of rows
         public string[][] FileMatrix; //matrix of the file readed
         public int threadCounter = 3; //number of thread
+        public Object thisLock = new Object(); //object lock critical section
+        public string FileBuff;
+        //INVOKE
+        public static void UiInvoke(Action a)
+        {
+            Application.Current.Dispatcher.Invoke(a);
+        }
 
         private void Open_File_Click(object sender, RoutedEventArgs e)
         {
-            string FileBuff; //buffer for read the file
+            //string FileBuff; //buffer for read the file
             OpenFileDialog openFileDialog = new OpenFileDialog();
             if (openFileDialog.ShowDialog() == true)
             {
@@ -48,9 +55,9 @@ namespace FinalProject
             }
         }
 
-        private void jccard_button_Click(object sender, RoutedEventArgs e)
+        public void calc_JDistance(int start, int end, int numOfThread)
         {
-             IEnumerable<string> union, intersect;
+            IEnumerable<string> union, intersect;
             float interCount = 0, unionCount = 0;
             float[][] tempDis;
             string[][] temp;
@@ -79,15 +86,39 @@ namespace FinalProject
             lock (thisLock)
             {
                 Array.Copy(tempDis, start, Jdistance, start, end - start);
-                threadCounter--; 
-                UiInvoke(() => txtEditor.Text += "Thread number " + numOfThread +" is finish\n");
+                threadCounter--;
+                UiInvoke(() => txtEditor.Text += "Thread number " + numOfThread + " is finish\n");
                 if (threadCounter == 0)
                 {
                     MessageBox.Show("Finish - Jaccard distance", "Thread", MessageBoxButton.OK, MessageBoxImage.Information);
                     UiInvoke(() => txtEditor.Text = String.Join(" ", Jdistance[0].Select(p => p.ToString()).ToArray()));
                 }
 
+            }
+        }
+
+        private void jccard_button_Click(object sender, RoutedEventArgs e)
+        {
+            string[] lines = FileBuff.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.None);
+            FileMatrix = new string[lines.Length][];
+            for (int i = 0; i < lines.Length; i++)
+            {
+                FileMatrix[i] = lines[i].Split(new string[] { "\t", " " }, StringSplitOptions.None);
+            }
+
+            Jdistance = new float[lines.Length][];
+            linesNumber = lines.Length;
+            //Thread creation
+            Thread tt1 = new Thread(() => calc_JDistance(0, linesNumber / 5, 1));
+            Thread tt2 = new Thread(() => calc_JDistance(linesNumber / 5, (linesNumber / 5) * 2, 2));
+            Thread tt3 = new Thread(() => calc_JDistance((linesNumber / 5) * 2, linesNumber, 3));
+
+            tt1.Start();
+            tt2.Start();
+            tt3.Start();
+
         }
 
     }
+
 }
